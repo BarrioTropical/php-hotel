201885341bbcd1e76bb505cac9735ba564d14b8e
BarrioTropical <barriotropical@gmail.com>
2022-11-28 15:48:25 +0100

    print hotel from php
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- index.php
+++ index.php
@@ -1,20 +1,86 @@
 <?php
-$frase = 'test php';
+$frase = 'Hotel Rooms';
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Php Bad words</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+        crossorigin="anonymous"></script>
+    <title>Php Hotel Rooms Array</title>
 </head>
+
 <body>
-    
-<h1><?php echo $frase; ?></h1>
+    <h1><?php echo $frase; ?></h1>
+    <div class="row">
+        <?php
+        foreach ($hotels as $hotel) {
+        ?>
+        <div class="col-4 my-card">
+            <h2><?php echo $hotel['name'] ?></h2>
+            <p><?php echo $hotel['description'] ?></p>
+            <p><?php echo $hotel['parking'] ?></p>
+            <p>Vote: <?php echo $hotel['vote'] ?></p>
+            <p>Distance: <?php echo $hotel['distance_to_center'] ?> km </p>
+        </div>
+        <?php
+
+        } ?>
+
+
+
+    </div>
+
+
+
+    </div>
+
+
 </body>
+
 </html>
