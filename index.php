bad1fd78b769c6549daa9d4ded12bcfb79b6e367
BarrioTropical <barriotropical@gmail.com>
2022-11-28 13:55:48 +0100

    php MAMP config
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$frase = 'test php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Php Bad words</title>
+</head>
+<body>
+    
+<h1><?php echo $frase; ?></h1>
+</body>
+</html>
